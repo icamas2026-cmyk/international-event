b9997387c1002da4f1a3326f0784eb8fef85ea21
abhi22102004@gmail.com <jeet22102004@gmail.com>
2025-09-06 09:38:52 +0530

    Updated package.json and UnifiedClone.jsx

--- src/UnifiedClone.jsx
+++ src/UnifiedClone.jsx
@@ -124,8 +124,7 @@ export default function UnifiedClone() {
               className="text-3xl md:text-6xl font-bold leading-tight mb-6"
             >
               1st International Conference on Advances in Mechanical and
-              Aerospace Science 
-              (ICAMAS-2026)
+              Aerospace Science (ICAMAS-2026)
             </motion.h1>
             <motion.p
               initial={{ opacity: 0 }}
@@ -134,7 +133,15 @@ export default function UnifiedClone() {
               className="text-xl md:text-2xl"
             >
               Conference dates: 10-12 March, 2026
-              All accepted & presented papers will be published in Scopus indexed conference proceedings
+              <motion.p
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                transition={{ duration: 1, delay: 0.5 }}
+                className="text-xl md:text-2xl"
+              >
+                All accepted & presented papers will be published in Scopus
+                indexed conference proceedings
+              </motion.p>
             </motion.p>
           </div>
         </section>
